d2bbe1cd85ff3d39e8bd41b6a9ed07daa1f9a682
Erica <erbarone@microsoft.com>
2021-05-10 16:45:27 +0200

    adds vs solution

---
+++ AppsettingsSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace AppsettingsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get the environment variable
+            var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile($"appsettings.{environment}.json", true, true)
+                .Build();
+
+            // access the properties of the SectionConfig class
+            var section = config.GetSection(nameof(SectionConfig));
+            var settings = section.Get<SectionConfig>();
+
+            Console.WriteLine($"URL is: {settings.URL}");
+            Console.WriteLine($"Key is: {settings.SecretKey}");
+
+            // access the properties with no associated class
+            var prop = config["SectionWithNoClass:Property"];
+
+            Console.WriteLine($"With no class: {prop}");
+
+            // Application code should start here.
+        }
+    }
+}
---
+++ AppsettingsSample/SectionConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace AppsettingsSample
+{
+    public class SectionConfig
+    {
+        public string URL { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
